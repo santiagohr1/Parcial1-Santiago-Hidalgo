970b32561f3d73d69c19b1ef7b5c3747e1330b72
santiagohr1 <u20232217373@usco.edu.co>
2026-03-09 19:12:55 -0500

    Feature
    nueva caracteristica en la lista de productos

---
+++ parcial1-master/js/script.js
@@ -0,0 +1,34 @@
+function cambiarTexto() {
+
+    document.getElementByID("descripcion").innerHTML = "Ahora estás viendo nuestras ofertas!"
+
+}
+
+function modoOscuro() {
+
+    document.body.style.backgroundColor = "black"
+    document.body.style.color = "white"
+
+}
+
+let carrito = 0
+
+function agregarCarrito() {
+
+    carrito++
+    console.log("Productos en carrito: " + carrito)
+
+}
+
+function enviarFormulario() {
+
+    let nombre = document.getElementById("nombre").value
+    let correo = document.getElementById("correo").value
+
+    if (nombre == "") {
+        alert("El nombre es obligatorio")
+    }
+
+    console.log("Formulario enviado")
+
+}
